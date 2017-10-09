f23576910e85f6d75922b14f9b234d2dc79d3f02
Leonardo Mendes <Leoosaid>
2017-10-08 23:44:51 -0300

    Atualização do projeto

--- src/test/java/br/com/continuous/integration/test/AppTest.java
+++ src/test/java/br/com/continuous/integration/test/AppTest.java
@@ -35,15 +35,15 @@ import br.com.continuous.integration.utils.ServicePath;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AppTest{
-	
+public class AppTest {
+
 	private static final Logger LOGGER = Logger.getLogger(AppTest.class);
 
 	@Autowired
 	private UsuarioController usuarioController;
 
 	private MockMvc mockMvc;
-	
+
 	@Before
 	public void init() throws IOException {
 		mockMvc = MockMvcBuilders.standaloneSetup(usuarioController).setControllerAdvice(new Exception()).build();
@@ -51,105 +51,96 @@ public class AppTest{
 
 	@Test
 	public void enviandoUmJsonVazioBadRequest() throws Exception {
-		mockMvc.perform(post(ServicePath.USER_PATH + "/")
-						.contentType(APPLICATION_JSON)
-						.content(new JSONObject().toString().getBytes()))
-				.andDo(print())
-				.andExpect(status().isBadRequest());
+		mockMvc.perform(post(ServicePath.USER_PATH + "/").contentType(APPLICATION_JSON)
+				.content(new JSONObject().toString().getBytes())).andDo(print()).andExpect(status().isBadRequest());
 
 	}
 
 	@Test
 	public void salvandoUsuarioOk() throws Exception {
-		mockMvc.perform(post(ServicePath.USER_PATH + "/")
-						.contentType(APPLICATION_JSON)
-						.content(buildJsonAsByte(false)))
-				.andDo(print())
-				.andExpect(status().isOk());
+		mockMvc.perform(post(ServicePath.USER_PATH + "/").contentType(APPLICATION_JSON).content(buildJsonAsByte(false)))
+				.andDo(print()).andExpect(status().isOk());
 
 	}
-	
+
 	@Test
 	public void buscandoUsuarioOk() throws Exception {
-		mockMvc.perform(get(ServicePath.USER_PATH + "/id/10")
-						.accept(APPLICATION_JSON))
-				.andDo(print())
+		mockMvc.perform(get(ServicePath.USER_PATH + "/id/10").accept(APPLICATION_JSON)).andDo(print())
 				.andExpect(status().isOk()).andExpect(content().contentType(APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("nome", is("leonardo")))
-				.andExpect(jsonPath("email", is("leonardo@gmail.com")));
+				.andExpect(jsonPath("nome", is("leonardo"))).andExpect(jsonPath("email", is("leonardo@gmail.com")));
 	}
-	
+
 	@Test
 	public void atualizandoUsuarioOk() throws Exception {
-		mockMvc.perform(put(ServicePath.USER_PATH + "/")
-				.contentType(APPLICATION_JSON)
-				.content(buildJsonAsByte(true)))
-		.andDo(print())
-		.andExpect(status().isOk());
-		
+		mockMvc.perform(put(ServicePath.USER_PATH + "/").contentType(APPLICATION_JSON).content(buildJsonAsByte(true)))
+				.andDo(print()).andExpect(status().isOk());
+
 	}
-	
+
+	@Test
+	public void atualizandoUsuarioBadRequest() throws Exception {
+		mockMvc.perform(put(ServicePath.USER_PATH + "/").contentType(APPLICATION_JSON)
+				.content(new JSONObject().toString().getBytes())).andDo(print()).andExpect(status().isBadRequest());
+
+	}
+
 	@Test
 	public void printInfo() throws Exception {
 		mockMvc.perform(get(ServicePath.USER_PATH + "/id/1")).andDo(print());
 
 	}
-	
+
 	@Test
 	public void testUf() throws Exception {
 		LOGGER.info(Uf.getSerialversionuid());
 	}
-	
+
 	@Test
 	public void removendoUsuarioOk() throws Exception {
-		mockMvc.perform(delete(ServicePath.USER_PATH + "/id/10")
-				.accept(APPLICATION_JSON))
-		.andDo(print())
-		.andExpect(status().isOk());
+		mockMvc.perform(delete(ServicePath.USER_PATH + "/id/10").accept(APPLICATION_JSON)).andDo(print())
+				.andExpect(status().isOk());
 	}
-	
+
 	@Test
 	public void removendoUsuarioBadRequest() throws Exception {
-		mockMvc.perform(delete(ServicePath.USER_PATH + "/id/67")
-				.accept(APPLICATION_JSON))
-		.andDo(print())
-		.andExpect(status().isBadRequest());
+		mockMvc.perform(delete(ServicePath.USER_PATH + "/id/67").accept(APPLICATION_JSON)).andDo(print())
+				.andExpect(status().isBadRequest());
 	}
 
 	private byte[] buildJsonAsByte(boolean isUpdate) throws JSONException {
-		
-		JSONObject ufJson = new  JSONObject();
+
+		JSONObject ufJson = new JSONObject();
 		ufJson.put("id", 13);
 		ufJson.put("sigla", "SP");
-		
-		JSONObject cidadeJson = new  JSONObject();
+
+		JSONObject cidadeJson = new JSONObject();
 		cidadeJson.put("id", 12);
 		cidadeJson.put("nome", "São Paulo");
 		cidadeJson.put("uf", ufJson);
-		
-		JSONObject bairroJson = new  JSONObject();
+
+		JSONObject bairroJson = new JSONObject();
 		bairroJson.put("cidade", cidadeJson);
 		bairroJson.put("id", 11);
 		bairroJson.put("nome", "São Paulo");
-		
-		JSONObject enderecoJson = new  JSONObject();
+
+		JSONObject enderecoJson = new JSONObject();
 		enderecoJson.put("id", 10);
 		enderecoJson.put("nome", "Alemanha");
 		enderecoJson.put("cep", "38410250");
 		enderecoJson.put("bairro", bairroJson);
-		
+
 		JSONObject jsonObject = new JSONObject();
-		
-		if(isUpdate) {
+
+		if (isUpdate) {
 			jsonObject.put("id", 10);
 		}
-		
+
 		jsonObject.put("nome", "leonardo");
 		jsonObject.put("telefone", "99143829");
 		jsonObject.put("email", "leonardo@gmail.com");
 		jsonObject.put("numero", 121);
 		jsonObject.put("endereco", enderecoJson);
-		
+
 		return jsonObject.toString().getBytes();
 	}
 }
